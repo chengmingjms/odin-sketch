bb46f6194d52d6c22fc47c61407e6b4e241bfbd5
Jean-Marie Schlömer <chengmingjms@gmail.com>
2024-04-12 21:56:07 +0700

    Added new grid button and its logic

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 var rows = 16;
 const containerDiv = document.querySelector("#container");
-for (var i = 0; i < rows; i++) {
-    var rowDiv = document.createElement("div");
-    rowDiv.classList.add("insideContainer");
-    containerDiv.appendChild(rowDiv);
-    for (var j = 0; j < rows; j++) {
-        var newDiv = document.createElement("div");
-        newDiv.classList.add("square");
-        newDiv.addEventListener("mouseover", function(e) {e.target.style.backgroundColor = "black"})
-        rowDiv.appendChild(newDiv);
+const newGridBtn = document.querySelector("#newGridBtn")
+newGridBtn.addEventListener("click", function () {
+    var size = prompt("How big should the square be? (1-100)")
+    clearGrid()
+    createGrid(size)
+})
+createGrid(rows)
+
+function createGrid(rows) {
+    for (var i = 0; i < rows; i++) {
+        var rowDiv = document.createElement("div");
+        rowDiv.classList.add("insideContainer");
+        containerDiv.appendChild(rowDiv);
+        for (var j = 0; j < rows; j++) {
+            var newDiv = document.createElement("div");
+            newDiv.classList.add("square");
+            newDiv.addEventListener("mouseover", function(e) {e.target.style.backgroundColor = "black"})
+            rowDiv.appendChild(newDiv);
+        }
+    }
+}
+
+function clearGrid() {
+    while (containerDiv.firstChild) {
+        containerDiv.removeChild(containerDiv.lastChild);
     }
 }
