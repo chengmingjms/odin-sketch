c9e5e913a2254b9ca1cc165b037965a9cb856e44
Jean-Marie Schlömer <chengmingjms@gmail.com>
2024-04-12 21:39:42 +0700

    Added size var in CSS and row divs in HTML

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 var rows = 16;
 const containerDiv = document.querySelector("#container");
 for (var i = 0; i < rows; i++) {
+    var rowDiv = document.createElement("div");
+    rowDiv.classList.add("insideContainer");
+    containerDiv.appendChild(rowDiv);
     for (var j = 0; j < rows; j++) {
         var newDiv = document.createElement("div");
         newDiv.classList.add("square");
-        containerDiv.appendChild(newDiv);
+        rowDiv.appendChild(newDiv);
     }
 }
